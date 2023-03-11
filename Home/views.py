2be93c73e94475839abb8275c8d92c22d3ca5f77
Raphael CAUSSE <raphael.causse2@gmail.com>
2023-03-11 20:10:11 +0100

    add: headers and pages from lucas 49ffd5f

--- Home/views.py
+++ Home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
+    """ Render index page (when user is not connected). """
     return render(request, 'index.html')
 
-def user(request):
-    return render(request, 'user.html')
+def home(request):
+    """ Render home page (when user is connected). """
+    return render(request, 'home.html')
